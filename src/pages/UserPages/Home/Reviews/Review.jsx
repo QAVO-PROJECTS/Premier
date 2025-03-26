dd2aa8981ffd8ed415911023295cd9ce716545a5
AnarZulfugarov <anarrz-af106@code.edu.az>
2025-03-26 17:59:40 +0400

    fix problem

--- src/pages/UserPages/Home/Reviews/Review.jsx
+++ src/pages/UserPages/Home/Reviews/Review.jsx
@@ -1,30 +1,24 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import "./review.scss";
 import plane from "/src/assets/plane.png";
 import Index from "../../../../components/UserComponents/CurvedSlider/index.jsx";
 import { useTranslation } from 'react-i18next';
-import { useGetAllCustomerViewsQuery } from "../../../../services/adminApi.jsx";
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import {useGetAllCustomerViewsQuery} from "../../../../services/adminApi.jsx";
 
 function Review() {
     const { t } = useTranslation();
 
-    useEffect(() => {
-        AOS.init({ duration: 1000 });
-    }, []);
-
     return (
-        <div className="review" data-aos="fade-up">
-            <div className="container" data-aos="fade-in">
-                <div className="title" data-aos="fade-right">
+        <div className={"review"}>
+            <div className={"container"}>
+                <div className={"title"}>
                     <h2>{t("home.review.title", "Müştəri Rəyləri")}</h2>
                 </div>
-                <div data-aos="zoom-in">
+                <div>
                     <Index />
                 </div>
-                <div className="text-center" data-aos="flip-up">
-                    <img src={plane} alt="" className="reviewImg" />
+                <div className={"text-center"}>
+                    <img src={plane} alt="" className={"reviewImg"} />
                 </div>
             </div>
         </div>
